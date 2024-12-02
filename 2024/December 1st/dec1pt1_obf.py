7d9f449b89efe8d754982b5767b31ec1074c02d1
Nikolas Brooks <nbrooks@bytoa.com>
2024-12-02 14:52:22 -0500

    updated with unique bytoa account inputs

--- 2024/December 1st/dec1pt1_obf.py
+++ 2024/December 1st/dec1pt1_obf.py
@@ -1,2 +1,2 @@
 print(f"{sum(abs(x-y)for x,y in zip(*[sorted(l)for l in zip(*[[*map(int,z.split())]for z in open('dec1input.txt')])]))}")
-# 2264607
+# 1590491
--- 2024/December 1st/dec1pt2_obf.py
+++ 2024/December 1st/dec1pt2_obf.py
@@ -1,3 +1,3 @@
 from collections import Counter as C
 print(f"{sum((l*(d:=C(b:=[*zip(*[map(int,x.split())for x in open('dec1input.txt')])][1]))[l])for l in sorted([*zip(*[map(int,x.split())for x in open('dec1input.txt')])][0]))}")
-# 19457120
+# 22588371
